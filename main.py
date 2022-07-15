3ac379a7f4e3d605ec22a9f475aca55c13e459fe
ClaudioFoti <clau.foti@gmail.com>
2022-07-15 13:45:25 +0200

    feat: added /health route

--- main.py
+++ main.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def index():
     return "Congratulations, it's a web app!"
 
+@app.route("/host_id")
+def host_id():
+    return "UUID"
+
+@app.route("/health")
+def health():
+    return "health"
+
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
